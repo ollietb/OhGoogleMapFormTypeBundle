e29f7d6e63e6206bdc47b352f88344ce6f07e9e9
Leszek Prabucki <leszek.prabucki@gmail.com>
2012-08-08 14:29:09 +0300

    Fixed form type which do not work with sf2.1 RC
    see https://github.com/symfony/symfony/blob/master/src/Symfony/Component/Form/FormTypeInterface.php#L50
    
    Declaration of Oh\GoogleMapFormTypeBundle\Form\Type\GoogleMapType::buildView() must be compatible with Symfony\Component\Form\FormTypeInterface::buildView

--- Form/Type/GoogleMapType.php
+++ Form/Type/GoogleMapType.php
@@ -5,26 +5,25 @@ namespace Oh\GoogleMapFormTypeBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class GoogleMapType extends AbstractType
 {
-	
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
         $builder
             ->add($options['lat_name'], $options['type'], array_merge($options['options'], $options['lat_options']))
             ->add($options['lng_name'], $options['type'], array_merge($options['options'], $options['lng_options']))
         ;
     }
-	
+
     /**
      * {@inheritdoc}
      */
@@ -38,33 +37,32 @@ class GoogleMapType extends AbstractType
             'lat_name'       => 'lat',   // the name of the lat field
             'lng_name'       => 'lng',   // the name of the lng field
             'error_bubbling' => false,
-			'map_width'      => 300,     // the width of the map
-			'map_height'     => 300,     // the height of the map
-			'default_lat'    => 51.5,    // the starting position on the map
-			'default_lng'    => -0.1245, // the starting position on the map
-			'include_jquery' => false,   // jquery needs to be included above the field (ie not at the bottom of the page)
-			'include_gmaps_js'=>true     // is this the best place to include the google maps javascript?
+            'map_width'      => 300,     // the width of the map
+            'map_height'     => 300,     // the height of the map
+            'default_lat'    => 51.5,    // the starting position on the map
+            'default_lng'    => -0.1245, // the starting position on the map
+            'include_jquery' => false,   // jquery needs to be included above the field (ie not at the bottom of the page)
+            'include_gmaps_js'=>true     // is this the best place to include the google maps javascript?
         ));
     }
-	
+
     /**
      * {@inheritdoc}
      */
-    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
-		$view
-			->setVar('lat_name', $options['lat_name'])
-			->setVar('lng_name', $options['lng_name'])
-			->setVar('map_width', $options['map_width'])
-			->setVar('map_height', $options['map_height'])
-			->setVar('default_lat', $options['default_lat'])
-			->setVar('default_lng', $options['default_lng'])
-			->setVar('include_jquery', $options['include_jquery'])
-			->setVar('include_gmaps_js', $options['include_gmaps_js'])
-		;
-		
+        $view
+            ->setVar('lat_name', $options['lat_name'])
+            ->setVar('lng_name', $options['lng_name'])
+            ->setVar('map_width', $options['map_width'])
+            ->setVar('map_height', $options['map_height'])
+            ->setVar('default_lat', $options['default_lat'])
+            ->setVar('default_lng', $options['default_lng'])
+            ->setVar('include_jquery', $options['include_jquery'])
+            ->setVar('include_gmaps_js', $options['include_gmaps_js'])
+        ;
     }
-	
+
     public function getParent()
     {
         return 'form';
